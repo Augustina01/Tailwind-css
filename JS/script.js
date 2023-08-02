f80e5eb0dd5521f36c9daddbc704298c06faa5e5
Augustina01 <oluwabusola.aoa@gmail.com>
2023-08-02 18:11:39 +0100

    picture gallery commit

---
+++ JS/script.js
@@ -0,0 +1,73 @@
+//Including event to the image title
+//creating an event for image 1
+const change = document.getElementById("image1");
+change.addEventListener("click", (event) =>{
+    event.target.style.background="brown";
+    event.target.style.color="white";
+});
+//creating an event for image 2
+const change2 = document.getElementById("image2");
+change2.addEventListener("click", (event) =>{
+    event.target.style.background="blue";
+    event.target.style.color="white";
+});
+//creating an event for image 3
+const change3 = document.getElementById("image3");
+change3.addEventListener("click", (event) =>{
+    event.target.style.background="green";
+    event.target.style.color="white";
+});
+//creating an event for image 4
+const change4 = document.getElementById("image4");
+change4.addEventListener("click", (event) =>{
+    event.target.style.background="yellow";
+    event.target.style.color="white";
+});
+//creating an event for image 5
+const change5 = document.getElementById("image5");
+change5.addEventListener("click", (event) =>{
+    event.target.style.background="pink";
+    event.target.style.color="white";
+});
+//creating an event for image 6
+const change6 = document.getElementById("image6");
+change6.addEventListener("click", (event) =>{
+    event.target.style.background="purple";
+    event.target.style.color="white";
+});
+//creating an event for image 7
+const change7 = document.getElementById("image7");
+change7.addEventListener("click", (event) =>{
+    event.target.style.background="red";
+    event.target.style.color="white";
+});
+//creating an event for image 8
+const change8 = document.getElementById("image8");
+change8.addEventListener("click", (event) =>{
+    event.target.style.background="teal";
+    event.target.style.color="white";
+});
+//creating an event for image 9
+const change9 = document.getElementById("image9");
+change9.addEventListener("click", (event) =>{
+    event.target.style.background="orange";
+    event.target.style.color="white";
+});
+//creating an event for image 10
+const change10 = document.getElementById("image10");
+change10.addEventListener("click", (event) =>{
+    event.target.style.background="brown";
+    event.target.style.color="white";
+});
+//creating an event for image 11
+const change11 = document.getElementById("image11");
+change11.addEventListener("click", (event) =>{
+    event.target.style.background="black";
+    event.target.style.color="white";
+});
+// creating an event for image 12
+const change12 = document.getElementById("image12");
+change12.addEventListener("click", (event) =>{
+    event.target.style.background="indigo";
+    event.target.style.color="white";
+});
